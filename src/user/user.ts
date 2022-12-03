f0e02bb9943ee705cd023de03088c221c7e30989
GeorgeRNG <grogrng@gmail.com>
2022-12-03 20:02:43 +0000

    Time to implement this

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,3 +1,5 @@
+import { randomBytes, createHash } from 'crypto';
+
 export type username = string;
 export type uuid     = string;
 
@@ -17,11 +19,30 @@ export class UserAccess {
     clientCode: string;
     /** We give the connecting user our server code */
     serverCode: string;
-    /** We merge and hash the client and server code and export it as hex.
-     * This should only exist if the user is known to be valid.
+    /** 
+     * We merge and hash the client and server code and export it as hex.  
+     * This should only exist if the user is known to be valid.  
+     * This will be the bearer token.  
      * ```js
      * SHA256(client,server).hex()
      * ```
      */
-    agreedCode: string;
+    commonCode: string | null;
+
+    constructor(clientCode: string) {
+        this.clientCode = clientCode;
+        this.serverCode = randomBytes(16).toString('hex');
+    }
+    validate(user: User) {
+        this.user = user;
+        this.commonCode = createHash('sha256').update(this.clientCode).update(this.serverCode).digest('hex');
+    }
+
+    get isValid() {
+        return this.serverCode != null;
+    }
+}
+
+export class UserAccessTable {
+    private users: UserAccess[];
 }
--- src/util/MojangAPI.ts
+++ src/util/MojangAPI.ts
@@ -1,9 +1,10 @@
-import { NetworkError } from "../Errors.js";
+import { NetworkError, RateLimitError } from "../Errors.js";
 import { username, uuid } from "../user/user.js";
 
 export async function getUserName(uuid: uuid): Promise<username> {
     const request = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid.replace(/-/g,'')}`);
-    if(request.status !== 200) throw new NetworkError(`Unexpected request status (${request.status})`);
+    if(request.status === 429) throw new RateLimitError(`Rate limited by Mojang`);
+    if(request.status !== 200) throw new NetworkError  (`Unexpected request status (${request.status})`);
     const body = await request.json();
     if(!('name' in body)) throw new TypeError('Could not find `name` in the body');
     return body.name;
