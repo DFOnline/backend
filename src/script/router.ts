eb4275819a250925078893da0b10ae42513c69ef
GeorgeRNG <grogrng@gmail.com>
2023-01-07 13:03:37 +0000

    WIP verify

--- src/script/router.ts
+++ src/script/router.ts
@@ -1,16 +1,28 @@
 import { Scripts } from "../index.js";
-import { Router, urlencoded } from "express";
+import { Response, Router, urlencoded } from "express";
+import { AuthorizedRequest } from "user/user.js";
 
 const router = Router();
 
 router.get('/',(_req,res) => {
-    res.send(Object.fromEntries(Scripts.get().map(script => [script.getID(), script])));
+    res.json(Object.fromEntries(Scripts.get().map(script => [script.getID(), script])));
 })
 
-router.get('/:script',urlencoded({extended: true}), (req,res) => {
+router.get('/:script', urlencoded({extended: true}), (req,res) => {
     const id = req.params['script'];
     if(!Scripts.exists(id)) { res.status(404).send('No script with that ID'); return; }
-    res.send(Scripts.get(id));
+    res.json(Scripts.get(id));
 });
+
+router.get('/:script/verify', urlencoded({extended: true}), (req, res: Response) => {
+    const id = req.params['script'];
+    const user = (req as any as AuthorizedRequest).user;
+    console.log(user);
+    if(!Scripts.exists(id)) { res.status(404).send('No script with that ID'); return; }
+    const script = Scripts.get(id);
+    const verified = !script.getVerified()
+    script.setVerified(verified);
+    res.json({verified});
+})
 
 export default router;
